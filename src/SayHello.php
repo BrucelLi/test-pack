cb61211db4eafa3b31918e67a55f28abc6ba511b
lixiaolong <lixiaolong@zdongpin.com>
2018-04-24 10:30:09 +0800

    'test'

---
+++ src/SayHello.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/4/24 0024
+ * Time: 上午 10:19
+ */
+namespace Hello;
+
+class SayHello
+{
+    public static function world()
+    {
+        return 'Hello World!';
+    }
+}
---
+++ test.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/4/24 0024
+ * Time: 上午 10:24
+ */
+require_once __DIR__ . '/vendor/autoload.php';
+use Hello\SayHello;
+echo SayHello::world();
